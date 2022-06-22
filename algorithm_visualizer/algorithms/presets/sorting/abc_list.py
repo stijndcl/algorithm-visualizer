45eca9556d6e9d26a24787d6c82de26dade52912
stijndcl <declercq.stijn@outlook.com>
2022-06-23 01:25:37 +0200

    Bubble sort

--- algorithm_visualizer/algorithms/presets/sorting/abc_list.py
+++ algorithm_visualizer/algorithms/presets/sorting/abc_list.py
@@ -1,3 +1,4 @@
+import math
 from abc import ABC
 
 import pygame as pg
@@ -9,11 +10,34 @@ from algorithm_visualizer.graphics import Renderer
 class ListSortingAlgorithm(Algorithm, ABC):
     """Base class for list sorting algorithms, which abstracts some things away"""
 
-    data: list
+    data: list[int]
+    greatest: int
 
-    def __init__(self, data: list):
+    rectangle_colour: pg.Color = pg.Color(255, 255, 255)
+    current_rectangle_colour: pg.Color = pg.Color(255, 0, 0)
+
+    current_index: int = -1
+
+    def __init__(self, data: list[int]):
         self.data = data
+        self.greatest = max(self.data)
+        self.reset_state()
 
     def render_current_state(self, renderer: Renderer):
         renderer.fill_background()
+
+        rect_width = renderer.width // len(self.data)
+        height_factor = renderer.height / self.greatest
+
+        # Draw a rectangle for every data point
+        for i, data_point in enumerate(self.data):
+            if i == self.current_index:
+                colour = self.current_rectangle_colour
+            else:
+                colour = self.rectangle_colour
+
+            bar_height = math.ceil(data_point * height_factor)
+
+            renderer.draw_rectangle(i * rect_width, renderer.height - bar_height, rect_width, bar_height, colour)
+
         pg.display.update()
--- algorithm_visualizer/algorithms/presets/sorting/bubble_sort.py
+++ algorithm_visualizer/algorithms/presets/sorting/bubble_sort.py
@@ -1,3 +1,5 @@
+import random
+
 from algorithm_visualizer.algorithms.presets.sorting.abc_list import (
     ListSortingAlgorithm,
 )
@@ -8,7 +10,21 @@ class BubbleSort(ListSortingAlgorithm):
     """Bubble sort"""
 
     def start(self, renderer: Renderer):
-        pass
+        for i in range(len(self.data)):
+            for j in range(len(self.data) - i - 1):
+                self.current_index = j
+                self.render_current_state(renderer)
+
+                # If the next element is bigger, swap
+                if self.data[j] > self.data[j + 1]:
+                    self.data[j], self.data[j + 1] = self.data[j + 1], self.data[j]
+
+                self.render_current_state(renderer)
+
+        # Sorting is done, render final state
+        # Set current index back to -1 to remove the selection highlight
+        self.current_index = -1
+        self.render_current_state(renderer)
 
     def reset_state(self):
-        pass
+        random.shuffle(self.data)
